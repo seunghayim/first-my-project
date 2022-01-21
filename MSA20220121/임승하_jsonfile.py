195063124ac73d4c324ca277dedd38b89fea7e81
seunghayim <97645665+seunghayim@users.noreply.github.com>
2022-01-21 17:22:41 +0900

    Update 임승하_jsonfile.py

--- MSA20220121/임승하_jsonfile.py
+++ MSA20220121/임승하_jsonfile.py
@@ -1,13 +1,31 @@
+file - mydata.json 
+{
+    "empid": 12345678,
+    "name": "홍길동",
+    "info": [
+        {
+            "date1": "2022-01-21",
+            "home": "서울시"
+        },
+        {
+            "dep": "개발",
+            "email": "aaa@aaa.co.kr"
+        }
+    ]
+}
+
+-------------------------------------------------------------
+# 1. 예외 처리를 이용해 json 읽기
 import json
 
 try:
     fileName = open("datalab/datalab/mydata.json", "rb")
     tempData = json.load(fileName)
     resultData1 = tempData["name"]
-    resultData1 = tempData["age"]
-    resultData1 = tempData["address"]
-    resultData1 = tempData["email"]
-    resultData1 = tempData["empcheck"]
+    resultData2 = tempData["age"]
+    resultData3 = tempData["address"]
+    resultData4 = tempData["email"]
+    resultData5 = tempData["empcheck"]
 except NameError as ex:
     error = str(ex)
 except FileNotFoundError as ex:
@@ -18,6 +36,8 @@ else:
     fileName.close()
 
 
+# 2.예외 처리를 이용해 json 쓰기
+# json 데이터
 jsonData2 = {
         "empid": 12345678,
         "name" : "홍길동",
@@ -28,7 +48,7 @@ jsonData2 = {
     }
 
 
-
+# 예외처리
 try:
     fileName1 = open('datalab/datalab/mydata1.json', "w", encoding="utf-8")
     json.dump(jsonData2, fileName1, ensure_ascii=False, indent=4)
