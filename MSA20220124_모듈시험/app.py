363825e63a4aa6fe5147060900787843a92f656a
seunghayim <97645665+seunghayim@users.noreply.github.com>
2022-01-24 19:07:57 +0900

    Update app.py

--- MSA20220124_모듈시험/app.py
+++ MSA20220124_모듈시험/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-@app.route('/data1/')
+@app.route('/data1')
 def FlaskData():
     keyValue = r"V6lQYKtQODdLI8HPFUM%2FF1fi2FGeE%2FbXr142ghKq1Vfzozi42DXMWVzP9Hp9GI%2FI%2F1%2BbUud5Pa4JVwsu653Y8Q%3D%3D"
     address = r"%EC%A4%91%EB%9E%91%EA%B5%AC"
@@ -17,12 +17,10 @@ def FlaskData():
     dataURL += f"&cond%5BorgZipaddr%3A%3ALIKE%5D={address}"
     dataURL += f"&serviceKey={keyValue}"
     
-    dataResult = requests.get(dataURL)
-    return json.loads(str(dataResult))
+    
+    return requests.get(dataURL).json()
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
  
 
-데이터 안나옵니다..
+데이터가 한글로 안나옵니다.
